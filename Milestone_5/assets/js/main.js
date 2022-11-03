b15f3133dba63a640a7a15777a40d3a6982481bc
paoloevan <paoloev1900@gmail.com>
2022-11-03 16:33:53 +0100

    add last message of milestone 5

--- Milestone_5/assets/js/main.js
+++ Milestone_5/assets/js/main.js
@@ -1,6 +1,6 @@
 /**
-Cancella messaggio: cliccando sul messaggio appare un menu a tendina che
-permette di cancellare il messaggio selezionato
+Visualizzazione ora e ultimo messaggio
+inviato/ricevuto nella lista dei contatti
 */
 
 
