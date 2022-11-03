b9242950b9261713e35c6be189bbe2dcf340e5e1
paoloevan <paoloev1900@gmail.com>
2022-11-03 11:13:37 +0100

    add milestone 2

--- Milestone_2/assets/js/main.js
+++ Milestone_2/assets/js/main.js
@@ -1,6 +1,10 @@
 /**
+Aggiunta di un messaggio: l’utente scrive un testo nella parte bassa e digitando
+“enter” il testo viene aggiunto al thread sopra, come messaggio verde
 
-Click sul contatto mostra la conversazione del contatto cliccato */
+Risposta dall’interlocutore: ad ogni inserimento di un messaggio, l’utente riceverà
+un “ok” come risposta, che apparirà dopo 1 secondo.
+*/
 
 
 const { createApp } = Vue
